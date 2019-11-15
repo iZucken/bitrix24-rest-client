d677f9be0054e9a4a98ddd2596ad49ba1b9b7911
l.tarasov <l.tarasov@itcomgk.ru>
2019-11-15 12:00:40 +0300

    Downgraded required PHP version down to 7.0;

---
+++ src/rest/client/AbstractConnection.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace bitrix\rest\client;
+
+use bitrix\exception\BitrixServerException;
+use bitrix\exception\TransportException;
+use bitrix\exception\UndefinedBitrixServerException;
+use Exception;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Throwable;
+
+abstract class AbstractConnection
+{
+    /**
+     * @var Client
+     */
+    protected $client = null;
+
+    /**
+     * @param string $uri
+     * @param array  $parameters
+     * @return mixed
+     * @throws BitrixServerException
+     * @throws TransportException
+     * @throws UndefinedBitrixServerException
+     * @throws Throwable
+     */
+    public function call(string $uri, array $parameters = [])
+    {
+        try {
+            $response = $this->client->request('POST', $uri, [
+                RequestOptions::FORM_PARAMS => $parameters,
+            ]);
+        } catch (Throwable $exception) {
+            throw new TransportException("This exception should not be ever happening: " . $exception->getMessage());
+        }
+        try {
+            $decoded = \GuzzleHttp\json_decode($response->getBody()->getContents(), true);
+        } catch (Exception $exception) {
+            throw new TransportException("Failed to decode result: " . $exception->getMessage());
+        }
+        if (!empty($decoded['error'])||!empty($decoded['error_description'])) {
+            throw new BitrixServerException("{$decoded['error']}: {$decoded['error_description']}");
+        }
+        if ($response->getStatusCode() !== 200) {
+            throw new UndefinedBitrixServerException($response->getStatusCode() . ": " . $response->getReasonPhrase());
+        }
+        $result = $decoded['result'];
+        if (isset($decoded['total'])) {
+            $result = [
+                'result' => $decoded['result'],
+                'total'  => $decoded['total'],
+            ];
+            if (isset($decoded['next'])) {
+                $result['next'] = $decoded['next'];
+            }
+        }
+        return $result;
+    }
+}
