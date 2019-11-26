264613aee7ceaa45f43bc11a8cfe223565165185
n.zarubin <n.zarubin@itsirius.ru>
2019-11-26 13:34:47 +0300

    file storage + usage example

--- src/storage/File.php
+++ src/storage/File.php
@@ -2,51 +2,36 @@
 
 namespace bitrix\storage;
 
-use Psr\Log\LoggerInterface;
 
 /**
- * Simple Memcached storage
+ * File storage
  *
  * @package bitrix\storage
  */
-class Memcached implements Storage
+class File implements Storage
 {
-    /**
-     * @var \Memcached
-     */
-    private $cache;
-    /**
-     * @var string
-     */
-    private $prefix;
-    /**
-     * @var LoggerInterface
-     */
-    private $logger;
+    private $fileName = null;
+    private $data = [];
 
-    public function __construct(\Memcached $memcached, string $prefix, LoggerInterface $logger)
+    public function __construct($fileName)
     {
-        $this->prefix = $prefix;
-        $this->logger = $logger;
-        $this->cache = $memcached;
+        $this->fileName = $fileName;
+        if (file_exists($fileName)) {
+            $this->data = json_decode(file_get_contents($fileName), true);
+        }
     }
 
     public function set(string $key, $value)
     {
-        $this->logger->debug("Writing cache $this->prefix$key");
-        $set = $this->cache->set($this->prefix . $key, $value, 86400);
-        if ($set === false) {
-            $this->logger->warning("Failed to write cache $this->prefix$key");
-        }
+        $this->data[$key] = $value;
+        file_put_contents($this->fileName, json_encode($this->data));
     }
 
     public function get(string $key)
     {
-        $this->logger->debug("Reading cache $this->prefix$key");
-        $value = $this->cache->get($this->prefix . $key);
-        if ($this->cache->getResultCode() === $this->cache::RES_NOTFOUND) {
-            $this->logger->info("Cache miss $this->prefix$key");
+        if (!isset($this->data[$key])) {
+            return null;
         }
-        return $value;
+        return $this->data[$key];
     }
 }
