7c90ac27ddcee58f048073464210188288dd88e9
l.tarasov <l.tarasov@itcomgk.ru>
2019-12-09 11:46:18 +0300

    Fixed address endpoint;

--- src/endpoint/crm/Address.php
+++ src/endpoint/crm/Address.php
@@ -4,8 +4,13 @@
 namespace bitrix\endpoint\crm;
 
 
-use bitrix\endpoint\CrmCrud;
+use bitrix\endpoint\GenericListFilter;
+use bitrix\endpoint\Schema;
+use bitrix\exception\BitrixClientException;
 use bitrix\exception\BitrixException;
+use bitrix\exception\BitrixServerException;
+use bitrix\exception\InputValidationException;
+use bitrix\exception\NotFoundException;
 use bitrix\exception\TransportException;
 
 /**
@@ -13,8 +18,18 @@ use bitrix\exception\TransportException;
  *
  * @package endpoint
  */
-class Address extends CrmCrud
+class Address
 {
+    /**
+     * @var Schema
+     */
+    protected $schema;
+
+    function __construct(Schema $schema)
+    {
+        $this->schema = $schema;
+    }
+
     function getScopeName(): string
     {
         return "crm";
@@ -33,5 +48,87 @@ class Address extends CrmCrud
     function getScopeSettings(): array
     {
         return $this->schema->getSchema()['crm']['address'];
+    }
+
+    /**
+     * @param array $fields
+     * @return int
+     * @throws InputValidationException
+     * @throws BitrixException
+     * @throws TransportException
+     */
+    public function add(array $fields): int
+    {
+        $this->schema->assertValidFields($this->getScopeSettings()['fields'], $fields, false);
+        return $this->schema->client->call($this->getScopePath() . '.add', ['FIELDS' => $fields]);
+    }
+
+    /**
+     * NOTE: UNSET FIELDS WILL BE SAVED AS EMPTY!
+     *
+     * @param array $fields
+     * @return bool
+     * @throws BitrixClientException
+     * @throws BitrixException
+     * @throws BitrixServerException
+     * @throws InputValidationException
+     * @throws NotFoundException
+     * @throws TransportException
+     */
+    public function update(array $fields): bool
+    {
+        $this->schema->assertValidFields($this->getScopeSettings()['fields'], $fields, true);
+        try {
+            return $this->schema->client->call($this->getScopePath() . '.update', ["FIELDS" => $fields]);
+        } catch (BitrixServerException $exception) {
+            throw $this->convertNotFoundException($exception);
+        }
+    }
+
+    /**
+     * @param int $type
+     * @param int $entity
+     * @param int $entityType
+     * @return bool
+     * @throws BitrixClientException
+     * @throws BitrixServerException
+     * @throws NotFoundException
+     * @throws TransportException
+     */
+    public function delete(int $type, int $entity, int $entityType): bool
+    {
+        try {
+            return $this->schema->client->call($this->getScopePath() . '.delete', ["TYPE_ID" => $type,"ENTITY_ID" => $entity,"ENTITY_TYPE_ID" => $entityType]);
+        } catch (BitrixServerException $exception) {
+            throw $this->convertNotFoundException($exception);
+        }
+    }
+
+    /**
+     * @param BitrixServerException $exception
+     * @return BitrixServerException|NotFoundException
+     */
+    function convertNotFoundException(BitrixServerException $exception)
+    {
+        if (preg_match("~(не найде|not found)~i", $exception)) {
+            return new NotFoundException("Entity not found");
+        }
+        return $exception;
+    }
+
+    /**
+     * @param GenericListFilter $filter
+     * @return array
+     * @throws InputValidationException
+     * @throws BitrixException
+     * @throws TransportException
+     * @throws NotFoundException - when out of list bounds
+     */
+    public function list(GenericListFilter $filter): array
+    {
+        $this->schema->assertValidFilter($this->getScopeSettings()['fields'], $filter);
+        $list = $this->schema->client->call($this->getScopePath() . '.list', $filter->toFullMap());
+        $this->schema->assertListResponseInbound($filter, $list);
+        return $list;
     }
 }
